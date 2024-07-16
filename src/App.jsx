0556a261e458cab83e5c439362e945d83d3284bd
lotusphere <lotusphere.sea@gmail.com>
2024-07-15 21:12:53 -0700

    Add group list to app to test display

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import AddGroup from './views/AddGroup'
+import GroupList from './views/GroupList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <h1>Fanverse!</h1>
+      <GroupList />
       <AddGroup />
     </>
   )
