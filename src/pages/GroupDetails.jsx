9d6213474d9cbda02f836e2124cf1564d7587cfe
lotusphere <lotusphere.sea@gmail.com>
2024-07-16 22:37:36 -0700

    Add laoding and error message display on group details

---
+++ src/pages/GroupDetails.jsx
@@ -0,0 +1,49 @@
+import { useParams } from 'react-router'
+import { useState, useEffect } from 'react'
+import { supabase } from '../../supabase'
+
+function GroupDetails() {
+  const { id } = useParams()
+  const [group, setGroup] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  async function fetchGroup() {
+    const { data, error } = await supabase
+      .from('groups')
+      .select('*')
+      .eq('id', id)
+      .single()
+
+    if (error) {
+      setErrorMessage(error.message)
+    } else {
+      setGroup(data)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchGroup()
+  }, [id])
+
+
+  if (loading) return <p>Loading...</p>
+  if (errorMessage) return <p>Error: {errorMessage}</p>
+
+  return (
+    <div className="group">
+      <img src={group.image} alt={`img for ${group.name}`} />
+      <h3 className="name">{group.name}</h3>
+      <a href={group.url} target="_blank">
+        <button>TikTok</button>
+      </a>
+      <p className="description">{group.description}</p>
+
+      <button>Edit</button>
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </div>
+  )
+}
+
+export default GroupDetails
