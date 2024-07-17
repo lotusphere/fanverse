a48227df4e2f48469574f7ed04059b6c8050d995
lotusphere <lotusphere.sea@gmail.com>
2024-07-16 23:02:08 -0700

    Add edit route in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import AddGroup from './pages/AddGroup'
 import GroupList from './pages/GroupList'
 import GroupDetails from './pages/GroupDetails'
+import EditGroup from './pages/EditGroup'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/groups" element={<GroupList />} />
         <Route path="groups/new" element={<AddGroup />} />
         <Route path="groups/:id" element={<GroupDetails />} />
+        <Route path="groups/:id/edit" element={<EditGroup />} />
       </Routes>
     </BrowserRouter>
   )
