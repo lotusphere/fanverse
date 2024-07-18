2b77cc8821ba86a0ea429039adf3bd97286fce14
lotusphere <lotusphere.sea@gmail.com>
2024-07-17 23:21:55 -0700

    Update card styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,19 @@
 import { Link } from 'react-router-dom'
 import '@picocss/pico'
+import tiktok from '../assets/tiktok.svg'
 
 function Card({ group }) {
   return (
     <div className="card">
       <article>
         <header>
-          <p>{group.name}</p>
-          <a href={group.url} target="_blank">TikTok</a>
+          <div className="card-title">{group.name}</div>
+          <a href={group.url} target="_blank">
+            <img className="tiktok" src={tiktok} alt="logo for tiktok" />
+          </a>
         </header>
         <p className="description">{group.description}</p>
-        <img src={group.image} alt={`img for ${group.name}`} />
+        <img className="card-img" src={group.image} alt={`img for ${group.name}`} />
         <footer>
           <Link to={`/groups/${group.id}`}>Details</Link>
         </footer>
