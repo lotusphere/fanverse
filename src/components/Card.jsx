13867c492a7c31a4dbd06260802f8d320d134de7
lotusphere <lotusphere.sea@gmail.com>
2024-07-17 18:49:10 -0700

    Update card styles with picocss

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,20 @@
+import { Link } from 'react-router-dom'
+import '@picocss/pico'
+
 function Card({ group }) {
   return (
     <li className="card">
-      <h3 className="name">{group.name}</h3>
-      <p className="description">{group.description}</p>
-      <a href={group.url} target="_blank">
-        <button className="discography-btn">TikTok</button>
-      </a>
-      <img src={group.image} alt={`img for ${group.name}`} />
-      {/* <button onClick={() => onDelete(group.id)}>Delete</button> */}
+      <article>
+        <header>
+          <p>{group.name}</p>
+          <a href={group.url} target="_blank">TikTok</a>
+        </header>
+        <p className="description">{group.description}</p>
+        <img src={group.image} alt={`img for ${group.name}`} />
+        <footer>
+          <Link to={`/groups/${group.id}`}>Details</Link>
+        </footer>
+      </article>
     </li>
   )
 }
