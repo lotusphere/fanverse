839f2fc12f3436295f2c5e102d11b553b066ef73
lotusphere <lotusphere.sea@gmail.com>
2024-07-18 13:38:26 -0700

    Wrap app with group provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
 import AddGroup from './pages/AddGroup'
-import GroupList from './pages/GroupList'
+import Groups from './pages/Groups'
 import GroupDetails from './pages/GroupDetails'
 import EditGroup from './pages/EditGroup'
+import GroupProvider from './GroupContext'
+import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<GroupList />} />
-        <Route path="/groups" element={<GroupList />} />
-        <Route path="groups/new" element={<AddGroup />} />
-        <Route path="groups/:id" element={<GroupDetails />} />
-        <Route path="groups/:id/edit" element={<EditGroup />} />
-      </Routes>
-    </BrowserRouter>
+    <GroupProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Groups />} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="groups/new" element={<AddGroup />} />
+          <Route path="groups/:id" element={<GroupDetails />} />
+          <Route path="groups/:id/edit" element={<EditGroup />} />
+        </Routes>
+      </BrowserRouter>
+    </GroupProvider>
   )
 }
 
