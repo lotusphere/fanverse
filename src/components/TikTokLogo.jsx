772a799dd0a7a582e4d655075539627035849b50
lotusphere <lotusphere.sea@gmail.com>
2024-07-19 23:20:59 -0700

    Use fontawesome icons to implement tiktok logo

--- src/components/TikTokLogo.jsx
+++ src/components/TikTokLogo.jsx
@@ -1,10 +1,13 @@
-import tiktok from '../assets/tiktok.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTiktok } from '@fortawesome/free-brands-svg-icons'
 
 function TikTokLogo({ url }) {
   return (
-    <a href={url} target="_blank">
-      <img className="tiktok" src={tiktok} alt="logo for tiktok" />
-  </a>
+    <div className="social-logo">
+      <a href={url} target="_blank">
+        <FontAwesomeIcon icon={faTiktok} />
+      </a>
+    </div>
   )
 }
 
